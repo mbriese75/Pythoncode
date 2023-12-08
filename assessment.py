79cbb0644571a787bb48ff2ef547702f15040e0f
mbriese75 <mbriesejr@gmail.com>
2023-12-08 11:02:05 -0800

    Create assessment.py
    Is the number even or odd?

---
+++ assessment.py
@@ -0,0 +1,10 @@
+inp_num = input("enter a number: ")
+
+inp_num = int(inp_num)
+
+if inp_num == 0:
+    print(inp_num, "is Even")
+elif inp_num%2==0:
+    print(inp_num, "is Even")
+else:
+    print(inp_num, "is Odd")
